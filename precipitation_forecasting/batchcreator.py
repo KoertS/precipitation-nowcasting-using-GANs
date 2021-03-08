3a84990564acd2935a89d214ae6cabc6c27cba6d
Koert <KoertS@users.noreply.github.com>
2021-03-08 15:26:53 +0000

    Added cropping and padding function to batchcreator

--- precipitation_forecasting/batchcreator.py
+++ precipitation_forecasting/batchcreator.py
@@ -3,13 +3,18 @@ import tensorflow as tf
 from tensorflow import keras
 from datetime import datetime, timedelta
 
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+
 class DataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
     def __init__(self, list_IDs, batch_size=32, 
                  img_dim = (765, 700, 1), x_seq_size=3, y_seq_size=1, shuffle=True,
-                x_path = '/nobackup/users/schreurs/project_GAN/dataset_radar',
-                y_path = '/nobackup/users/schreurs/project_GAN/dataset_aart',
-                normalize_y=True):
+                x_path = '/nobackup/users/schreurs/project_GAN/dataset_radar_np',
+                y_path = '/nobackup/users/schreurs/project_GAN/dataset_aart_np',
+                normalize_y=False, crop_y=True, pad_x=True):
         'Initialization'
         self.inp_shape = (x_seq_size, *img_dim)
         self.out_shape = (y_seq_size, *img_dim)
@@ -20,6 +25,11 @@ class DataGenerator(keras.utils.Sequence):
         self.x_path = x_path
         self.y_path = y_path
         self.normalize_y = normalize_y
+        
+
+        self.crop_y = crop_y
+        self.pad_x = pad_x
+        
 
     def __len__(self):
         'Denotes the number of batches per epoch'
@@ -61,41 +71,65 @@ class DataGenerator(keras.utils.Sequence):
             # Store target image(s)
             for c in range(self.out_shape[0]):
                 y[i,c] = self.load_y(y_IDs[c])
-        
+                
+        if self.pad_x:
+            X = self.pad_along_axis(X)
         if self.normalize_y:
             y = self.zscore(y)
-            
+        
+        if self.crop_y:
+            y = self.crop_center(y)
         return X, y
     
     def load_x(self, x_ID):
-        path = self.x_path + '/RAD_NL25_RAC_RT_{}.h5'.format(x_ID)
-        with h5py.File(path, 'r') as f:
-            rain = f['image1']['image_data'][:]
-            # set masked values to 0
-            rain[rain == 65535] = 0
-            # Expand dimensions from (w,h) to (w,h,c=1)
-            rain = np.expand_dims(rain, axis=-1)
-            return rain
+        path = self.x_path + '/{}.npy'.format(x_ID)
+      
+        rain = np.load(path)
+        # set masked values to 0
+        rain[rain == 65535] = 0
+        # Expand dimensions from (w,h) to (w,h,c=1)
+        rain = np.expand_dims(rain, axis=-1)
+        
+        return rain
         
     def load_y(self, y_ID):
-        path = self.y_path + '/RAD_NL25_RAC_MFBS_EM_5min_{}.nc'.format(y_ID)
+        path = self.y_path + '/{}.npy'.format(y_ID)
 
-        with netCDF4.Dataset(path, 'r') as f:
-            rain = f['image1_image_data'][:].data[0]
-            # Convert to integers
+        rain = np.load(path)
+        # set masked values to 0
+        # Note that the data was converted to integers by multiplying with 100
+        rain[rain == 65535*100] = 0
+        # Expand dimensions from (w,h) to (w,h,c=1)
+        rain = np.expand_dims(rain, axis=-1)
             
-            # set masked values to 0
-            rain[rain > 65534] = 0
-            # Expand dimensions from (w,h) to (w,h,c=1)
-            rain = np.expand_dims(rain, axis=-1)
-            
-            return rain.data
+        return rain
         
     def zscore(self, x):
         MEAN = 0.7740296547051635
         STD = 37.88184326601481
 
         return (x-MEAN)/STD
+    
+    def crop_center(self, img,cropx=350,cropy=384):
+        # batch size, sequence, height, width, channels
+        # Only change height and width
+        _,_, y,x, _ = img.shape
+        startx = 20+x//2-(cropx//2)
+        starty = 40+y//2-(cropy//2)    
+        return img[:,:,starty:starty+cropy,startx:startx+cropx:,]
+    
+    def pad_along_axis(self, array, pad_size = 3, axis = 2):
+        '''
+        Pad input to be divisible by 4. 
+        height of 765 to 768
+        '''
+        if pad_size <= 0:
+            return array
+
+        npad = [(0, 0)] * array.ndim
+        npad[axis] = (0, pad_size)
+
+        return np.pad(array, pad_width=npad, mode='constant', constant_values=0)
 
 
 def get_list_IDs(start_dt, end_dt,x_seq_size=3,y_seq_size=1):
