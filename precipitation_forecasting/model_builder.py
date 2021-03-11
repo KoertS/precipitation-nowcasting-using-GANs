d24d33a399fbc5cf448075a4908fd3c7a0c456e9
Koert <KoertS@users.noreply.github.com>
2021-03-11 11:26:21 +0000

    Create GAN class that combines the generator and discriminator

--- precipitation_forecasting/model_builder.py
+++ precipitation_forecasting/model_builder.py
@@ -169,6 +169,96 @@ def build_generator():
     model = tf.keras.Model(inputs=input_seq, outputs=output, name='Generator')
     return model
 
+def build_discriminator():
+    input_seq = tf.keras.Input(shape=(1, 384, 350, 1))
+    
+    # Conv1
+    x = tf.keras.layers.Conv2D(filters=32, kernel_size=(5,5), strides=(3,3), padding='same', name='Conv1')(input_seq)
+    x = tf.keras.layers.LeakyReLU(0.2)(x)
+    
+    #Conv2
+    x = tf.keras.layers.Conv2D(filters=64, kernel_size=(3,3), strides=(3,3), padding='same', name='Conv2')(x)
+    x = tf.keras.layers.LeakyReLU(0.2)(x)
+    
+    
+    # Conv3
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=(3,3), strides=(3,3), padding='same', name='Conv3')(x)
+    x = tf.keras.layers.LeakyReLU(0.2)(x)
+     
+    # Conv4
+    x = tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(3,3), padding='same', name='Conv4')(x)
+    x = tf.keras.layers.LeakyReLU(0.2)(x)
+        
+    # Dense
+    x = tf.keras.layers.Flatten()(x)
+    output = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+        
+    model = tf.keras.Model(inputs=input_seq, outputs=output, name='Discriminator')
+    return model
 
+class GAN(tf.keras.Model):
+    def __init__(self):
+        super(GAN, self).__init__()
+        self.discriminator = build_discriminator()
+        self.generator = build_generator()
+ 
+    
+    def compile(self, d_optimizer, g_optimizer, loss_fn):
+        super(GAN, self).compile()
+        self.d_optimizer = d_optimizer
+        self.g_optimizer = g_optimizer
+        self.loss_fn = loss_fn
+        self.d_loss_metric = keras.metrics.Mean(name="d_loss")
+        self.g_loss_metric = keras.metrics.Mean(name="g_loss")
 
+    @property
+    def metrics(self):
+        return [self.d_loss_metric, self.g_loss_metric]
 
+    def train_step(self, batch):
+        xs, ys = batch
+        batch_size = tf.shape(xs)[0]
+
+        # Decode them to fake images
+        generated_images = self.generator(xs)
+
+        # Combine them with real images
+        combined_images = tf.concat([generated_images, ys], axis=0)
+
+        # Assemble labels discriminating real from fake images
+        labels = tf.concat(
+            [tf.ones((batch_size, 1)), tf.zeros((batch_size, 1))], axis=0
+        )
+        # Add random noise to the labels - important trick!
+        labels += 0.05 * tf.random.uniform(tf.shape(labels))
+
+        # Train the discriminator
+        with tf.GradientTape() as tape:
+            predictions = self.discriminator(combined_images)
+            d_loss = self.loss_fn(labels, predictions)
+        grads = tape.gradient(d_loss, self.discriminator.trainable_weights)
+        self.d_optimizer.apply_gradients(
+            zip(grads, self.discriminator.trainable_weights)
+        )
+
+        # Sample random points in the latent space
+        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+
+        # Assemble labels that say "all real images"
+        misleading_labels = tf.zeros((batch_size, 1))
+
+        # Train the generator (note that we should *not* update the weights
+        # of the discriminator)!
+        with tf.GradientTape() as tape:
+            predictions = self.discriminator(self.generator(random_latent_vectors))
+            g_loss = self.loss_fn(misleading_labels, predictions)
+        grads = tape.gradient(g_loss, self.generator.trainable_weights)
+        self.g_optimizer.apply_gradients(zip(grads, self.generator.trainable_weights))
+
+        # Update metrics
+        self.d_loss_metric.update_state(d_loss)
+        self.g_loss_metric.update_state(g_loss)
+        return {
+            "d_loss": self.d_loss_metric.result(),
+            "g_loss": self.g_loss_metric.result(),
+        }
