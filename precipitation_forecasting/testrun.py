fcea449c2b7bb8bb6c634a2bc3ad79dc72e5d37a
koertS <KoertS@users.noreply.github.com>
2021-04-26 14:48:32 +0200

    Added script to perform test run

---
+++ precipitation_forecasting/testrun.py
@@ -0,0 +1,34 @@
+from batchcreator import DataGenerator, get_list_IDs
+from datetime import datetime
+import tensorflow as tf
+from model_builder import build_generator, GAN
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+model = GAN(rnn_type='GRU')
+model.build((None,5, 768, 700, 1))
+model.compile()
+model.summary()
+
+
+batch_size = 8
+
+# Create list of IDs to retrieve
+x_seq_size=5
+y_seq_size=1
+
+start_dt = datetime(2019,6,4,0,0)
+end_dt =  datetime(2019,6,6,0,30)
+# Exclude samples were there is no rain in the input
+filter_no_rain = True
+list_IDs = get_list_IDs(start_dt, end_dt, x_seq_size, y_seq_size, filter_no_rain=filter_no_rain)
+
+
+generator = DataGenerator(list_IDs, batch_size=batch_size, x_seq_size=x_seq_size, 
+                                       y_seq_size=y_seq_size, norm_method='minmax')
+history = model.fit(generator, epochs=3)
+
+
+with open(config.path_data + 'trainHistoryDict', 'wb') as file_pi:
+    pickle.dump(history.history, file_pi)
