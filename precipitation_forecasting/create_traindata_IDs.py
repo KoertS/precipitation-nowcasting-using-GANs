d83d6974a96bc5532459c75cd82ab4b390983215
Koert <KoertS@users.noreply.github.com>
2021-05-25 14:54:39 +0000

    Added code to create list of file ids

---
+++ precipitation_forecasting/create_traindata_IDs.py
@@ -0,0 +1,18 @@
+from batchcreator import get_list_IDs
+from datetime import datetime
+import numpy as np
+
+
+
+start_dt = datetime(2019,6,1,0,0)
+end_dt =  datetime(2019,7,1,0,0)
+x_length = 6
+y_length = 1
+filter_no_rain = 'avg0.01mm'
+filename = 'list_IDs20190606_avg001mm.npy'
+
+list_IDs = get_list_IDs(start_dt, end_dt, x_length, y_length, filter_no_rain=filter_no_rain)
+print(list_IDs[-1])
+print(len(list_IDs))
+
+np.save(filename, list_IDs)
