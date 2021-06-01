Correct the calculation for average rain, now ignores masked pixels when computing average rain

precipitation_forecasting/heavyrain_labeler.py
@@ -13,9 +13,20 @@ import sys
 full_cmd_arguments = sys.argv
 
 year = 0
-if len(full_cmd_arguments) == 2:
-    year = full_cmd_arguments[-1]
+overwrite = False
+
+if len(full_cmd_arguments) >= 2:
+    year = full_cmd_arguments[1]
     print('Computing labels for the year {}'.format(year))
+    if len(full_cmd_arguments) >= 3:
+        o_arg = full_cmd_arguments[2]
+        
+        if o_arg == '-o':
+            print('Overwriting previous labels')
+            overwrite = True
+        else:
+            print('Invalid second argument. Expected -o (overwrite)')
+            exit()
 else:
     print('Missing argument year (example: rainyday_labeler.py 2019)')
     exit()
@@ -27,6 +38,12 @@ root = radar_dir + year
 files = sorted([name for path, subdirs, files in os.walk(root) for name in files])
 
 cluttermask = ~np.load('cluttermask.npy')
+   
+path = config.dir_rtcor +  '2019/01/{}201901010000.h5'.format(config.prefix_rtcor)
+with h5py.File(path, 'r') as f:
+    rain = f['image1']['image_data'][:]
+    mask = (rain == 65535)
+nr_unmasked_pixels = (765*700)-np.sum(mask+~cluttermask)    
 def is_rainy(rdr):    
     # Calculate gradien magnitudes
     vgrad= np.gradient(rdr)
@@ -39,8 +56,9 @@ def is_rainy(rdr):
     rdr_clean = rdr_clean*cleaned
 
     # Label as rainy
-    # If not many high gradients (clutter) and total precipitation above 30mm
-    if len(np.argwhere(mag>500)) < 130 and np.average(rdr_clean) > 1:
+    # If not many high gradients (clutter) and avg > 0.01mm
+    avg_rain = np.sum(rdr_clean)/nr_unmasked_pixels
+    if len(np.argwhere(mag>500)) < 130 and avg_rain > 1:
         return True
     return False
 
@@ -74,7 +92,7 @@ def make_dir(dir_name):
 make_dir(label_dir)
 make_dir(label_dir+year)
 for m in range (1,13):
-   make_dir(label_dir+year+'/{:02d}'.format(m))
+    make_dir(label_dir+year+'/{:02d}'.format(m))
 
 for f in tqdm(files):
     ts = f.replace(config.prefix_rtcor, '')
@@ -85,7 +103,7 @@ for f in tqdm(files):
 
     label_fn = label_dir + '{Y}/{m}/{ts}.npy'.format(Y=year, m=month, ts=ts)
 
-    if not os.path.isfile(label_fn):
+    if not os.path.isfile(label_fn) or overwrite:
         try:
             rdr = load_h5(radar_dir+'/{}/{}/{}'.format(year,month,f))
             rainy = is_rainy(rdr)