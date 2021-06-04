6536fc84e5f15484b109b507e2a6554baa3dc1cc
Koert Schreurs <kschreurs@cn13.science.ru.nl>
2021-06-04 11:56:32 +0200

    Changed settings testrun.py

--- precipitation_forecasting/testrun.py
+++ precipitation_forecasting/testrun.py
@@ -16,29 +16,27 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 run = wandb.init(project='precipitation-forecasting',
             config={
             'batch_size' : 8,
-            'epochs': 100,
+            'epochs': 50,
             'lr_g': 0.0001,
             'lr_d': 0.0001,
-            'l_g': 0.006,
+            'l_g': 0.003,
             'l_mse': 1,
             'g_cycles': 3,
             'noise_labels': 0,
             'x_length': 6,
             'y_length': 1,
-            'rnn_type': 'GRU',
+            'rnn_type': 'LSTM',
             'filter_no_rain': 'avg0.01mm',
-            'train_data': 'train2016_2018.npy',
+            'train_data': 'train2015_2018.npy',
             'val_data': 'val2019.npy',
             'architecture': 'AENN',
             'model': 'GAN',
-            'norm_method': None
+            'norm_method': None,
+            'server':  'RU'
         })
 config = wandb.config
 
 # Create generator for training
-#start_dt = datetime(2019,6,6,0,0)
-#end_dt =  datetime(2019,6,8,0,0)
-#list_IDs = get_list_IDs(start_dt, end_dt, config.x_length, config.y_length, filter_no_rain = config.filter_no_rain)
 list_IDs = np.load(config.train_data, allow_pickle = True)
 print('Samples in training set:')
 print(len(list_IDs))
@@ -46,23 +44,27 @@ print(len(list_IDs))
 generator = DataGenerator(list_IDs, batch_size=config.batch_size,
                           x_seq_size=config.x_length, y_seq_size=config.y_length,
                           norm_method=config.norm_method, load_from_npy=False)
-val_IDs = np.load(config.val_data, allow_pickle = True)
-print('Samples in validation set:')
-print(len(val_IDs))
 
-validation_generator = DataGenerator(val_IDs, batch_size = config.batch_size,
+if config.val_data:
+    val_IDs = np.load(config.val_data, allow_pickle = True)
+    print('Samples in validation set:')
+    print(len(val_IDs))
+
+    validation_generator = DataGenerator(val_IDs, batch_size = config.batch_size,
                                      x_seq_size = config.x_length, y_seq_size = config.y_length,
                                      norm_method = config.norm_method, load_from_npy = False)
+else:
+    validation_generator = None
 # Initialize model
 if config.model == 'GAN':
     model = GAN(rnn_type = config.rnn_type, x_length = config.x_length, y_length = config.y_length,
              architecture = config.architecture, g_cycles=config.g_cycles, noise_labels = config.noise_labels,
-                l_g = config.l_g, l_mse = config.l_mse)
+                l_g = config.l_g, l_mse = config.l_mse, norm_method = config.norm_method)
     model.compile(lr_g = config.lr_g, lr_d = config.lr_d)
     callbacks = [WandbCallback(), logger.ImageLogger(generator), logger.GradientLogger(generator)]
 else:
     model = build_generator(architecture=config.architecture, rnn_type=config.rnn_type, relu_alpha=0.2,
-			x_length = config.x_length, y_length = config.y_length)
+			x_length = config.x_length, y_length = config.y_length, norm_method = config.norm_method)
     opt = tf.keras.optimizers.Adam(learning_rate=config.lr_g)
     model.compile(loss='mse', metrics=['mse', 'mae'])
     callbacks = [WandbCallback(), logger.ImageLogger(generator)]
