7047b12b12560fadaf7dfce75417af9edea28a0d
Koert Schreurs <kschreurs@cn13.science.ru.nl>
2021-06-10 11:32:09 +0200

    Changed run settings

--- precipitation_forecasting/testrun.py
+++ precipitation_forecasting/testrun.py
@@ -16,22 +16,24 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 run = wandb.init(project='precipitation-forecasting',
             config={
             'batch_size' : 8,
-            'epochs': 50,
-            'lr_g': 0.0001,
-            'lr_d': 0.0001,
-            'l_g': 0.003,
-            'l_mse': 1,
+            'epochs': 20,
+            'lr_g': 0.001,
+            'lr_d': 0.001,
+            'l_g': 0.006,
+            'l_rec': 1,
             'g_cycles': 3,
             'noise_labels': 0,
             'x_length': 6,
             'y_length': 1,
-            'rnn_type': 'LSTM',
+            'rnn_type': 'GRU',
             'filter_no_rain': 'avg0.01mm',
-            'train_data': 'train2015_2018.npy',
-            'val_data': 'val2019.npy',
+            'train_data': 'list_IDs201906_avg001mm.npy',
+            'val_data': None,
             'architecture': 'AENN',
             'model': 'GAN',
-            'norm_method': None,
+            'norm_method': 'minmax',
+            'downscale256': False,
+            'convert_to_dbz': False,
             'server':  'RU'
         })
 config = wandb.config
@@ -43,7 +45,8 @@ print(len(list_IDs))
 
 generator = DataGenerator(list_IDs, batch_size=config.batch_size,
                           x_seq_size=config.x_length, y_seq_size=config.y_length,
-                          norm_method=config.norm_method, load_from_npy=False)
+                          norm_method = config.norm_method, load_from_npy=False,
+                          downscale256 = config.downscale256, convert_to_dbz = config.convert_to_dbz)
 
 if config.val_data:
     val_IDs = np.load(config.val_data, allow_pickle = True)
@@ -52,19 +55,20 @@ if config.val_data:
 
     validation_generator = DataGenerator(val_IDs, batch_size = config.batch_size,
                                      x_seq_size = config.x_length, y_seq_size = config.y_length,
-                                     norm_method = config.norm_method, load_from_npy = False)
+                                     norm_method = config.norm_method, load_from_npy = False,
+                                     downscale256 = config.downscale256, convert_to_dbz = config.convert_to_dbz)
 else:
     validation_generator = None
 # Initialize model
 if config.model == 'GAN':
     model = GAN(rnn_type = config.rnn_type, x_length = config.x_length, y_length = config.y_length,
              architecture = config.architecture, g_cycles=config.g_cycles, noise_labels = config.noise_labels,
-                l_g = config.l_g, l_mse = config.l_mse, norm_method = config.norm_method)
+                l_g = config.l_g, l_rec = config.l_rec, norm_method = config.norm_method, downscale256 = config.downscale256)
     model.compile(lr_g = config.lr_g, lr_d = config.lr_d)
     callbacks = [WandbCallback(), logger.ImageLogger(generator), logger.GradientLogger(generator)]
 else:
     model = build_generator(architecture=config.architecture, rnn_type=config.rnn_type, relu_alpha=0.2,
-			x_length = config.x_length, y_length = config.y_length, norm_method = config.norm_method)
+			x_length = config.x_length, y_length = config.y_length, norm_method = config.norm_method, downscale256 = config.downscale256)
     opt = tf.keras.optimizers.Adam(learning_rate=config.lr_g)
     model.compile(loss='mse', metrics=['mse', 'mae'])
     callbacks = [WandbCallback(), logger.ImageLogger(generator)]
