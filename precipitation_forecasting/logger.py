42d0da485e4de0c7631e6f9fb8be187bdd727274
Koert <KoertS@users.noreply.github.com>
2021-07-28 11:12:33 +0000

    Gradient calculation now on cGAN instead of GAN

--- precipitation_forecasting/logger.py
+++ precipitation_forecasting/logger.py
@@ -113,7 +113,8 @@ class GradientLogger(tf.keras.callbacks.Callback):
             adv_loss_frame = self.model.train_disc_frame(generated_images, misleading_labels, train = False)
             
             if self.model.y_length > 1:
-                adv_loss_seq = self.model.train_disc_seq(generated_images, misleading_labels, train = False)
+                seq_pred = tf.concat([xs, generated_images], axis=1)
+                adv_loss_seq = self.model.train_disc_seq(seq_pred, misleading_labels, train = False)
             else:
                 adv_loss_seq = adv_loss_frame
             g_loss_adv = adv_loss_frame + adv_loss_seq
