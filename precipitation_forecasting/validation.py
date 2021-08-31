85d06bc9c99698a415e97ef2e997e42f17dc1541
Koert <KoertS@users.noreply.github.com>
2021-08-31 13:02:08 +0000

    Updated validation code

--- precipitation_forecasting/validation.py
+++ precipitation_forecasting/validation.py
@@ -1,10 +1,14 @@
 from pysteps.verification.detcatscores import det_cat_fct_init, det_cat_fct_accum, det_cat_fct_compute, det_cat_fct_merge
 from pysteps.verification.detcontscores import det_cont_fct_init, det_cont_fct_accum, det_cont_fct_compute
+from pysteps.verification.spatialscores import fss_init, fss_accum, fss_compute
 import numpy as np
 from batchcreator import DataGenerator, undo_prep
 from model_builder import GAN
 import tensorflow as tf
 from tqdm import tqdm
+from pysteps_nowcast import get_mask_rtcor
+import config
+import os
 
 class Evaluator:
     '''
@@ -15,8 +19,10 @@ class Evaluator:
     thresholds: precipitation threshold to use for the categorical scores
     leadtimes: the leadtimes of the predictions
     save_after_n_sample: if higher than 0, the evaluator will save its dictionary after it has seen n samples. 
+    scales: The spatial scales used to compute FSS at different spatial levels
     '''
-    def __init__(self, nowcast_method = 'S-PROG', thresholds = [0.5, 5, 10, 30], leadtimes = [30,60,90], save_after_n_samples = 0):
+    def __init__(self, nowcast_method = 'S-PROG', thresholds = [0, 0.5, 1, 2, 5, 10, 30], 
+                 leadtimes = [30,60,90], save_after_n_samples = 0, scales = [1, 2, 4, 16, 32, 64]):
         self.nowcast_method = nowcast_method
     
         ## Create dictionaries to compute the model errors
@@ -24,9 +30,16 @@ class Evaluator:
         self.cat_dicts = np.array([[det_cat_fct_init(thr) for thr in thresholds] for _ in leadtimes])
         # For the MSE and MAE create dictonary per leadtime to accumulate the errors
         self.cont_dicts = np.array([det_cont_fct_init() for _ in leadtimes])
-    
+        
+        
+        self.fss_dicts = np.array([[[fss_init(thr = thr, scale = scale) 
+                                     for scale in scales] 
+                                    for thr in thresholds]
+                                   for _ in leadtimes])
+        
         self.leadtimes = leadtimes
         self.thresholds = thresholds
+        self.scales = scales
 
         self.save_after_n_samples = save_after_n_samples
         self.n_verifies = 0
@@ -34,9 +47,13 @@ class Evaluator:
     def verify(self, y, y_pred, leadtime):
         index = self.leadtimes.index(leadtime)
         for cat_dict in self.cat_dicts[index]:
-            det_cat_fct_accum(cat_dict, y, y_pred)
-        det_cont_fct_accum(self.cont_dicts[index], y, y_pred)
-
+            det_cat_fct_accum(cat_dict, obs = y, pred = y_pred)
+        det_cont_fct_accum(self.cont_dicts[index], obs = y, pred = y_pred)
+        
+        for i, fss_thr in enumerate(self.fss_dicts[index]):
+            for j, fss_scale in enumerate(fss_thr):
+                fss_accum(self.fss_dicts[index, i, j], X_o = y, X_f = y_pred)
+                
         # Make checkpount if model went through n samples
         self.n_verifies +=1
         # verify is called for each lead time
@@ -63,22 +80,31 @@ class Evaluator:
     def save_accum_scores(self, n_samples):
         np.save('results/cat_dicts_{}'.format(self.nowcast_method), self.cat_dicts)
         np.save('results/cont_dicts_{}'.format(self.nowcast_method), self.cont_dicts)
+        np.save('results/fss_dicts_{}'.format(self.nowcast_method), self.fss_dicts)
         np.save('results/n_sample_{}'.format(self.nowcast_method), n_samples)
 
     def load_accum_scores(self):
         self.cat_dicts = np.load('results/cat_dicts_{}.npy'.format(self.nowcast_method), allow_pickle=True)
         self.cont_dicts = np.load('results/cont_dicts_{}.npy'.format(self.nowcast_method), allow_pickle=True)
+        self.fss_dicts = np.load('results/fss_dicts_{}.npy'.format(self.nowcast_method), allow_pickle=True)
         self.n_verifies = 3 * np.load('results/n_sample_{}.npy'.format(self.nowcast_method))
        
     
-def validate_model(model, run_name, on_test_set = False):
+def validate_model(model, run_name, on_test_set = False, random_split=False, resize_method = tf.image.ResizeMethod.BILINEAR):
     if not on_test_set:
         dataset = 'datasets/val2019_3y_30m.npy'
         data_name = 'val'
+        if random_split:
+            dataset = 'datasets/val_randomsplit.npy'
+            data_name+='_randomsplit'
     else:
         dataset = 'datasets/test2020_3y_30m.npy'
         data_name = 'test'
-        
+        if random_split:
+            dataset = 'datasets/test_randomsplit.npy'
+            data_name+='_randomsplit'
+     
+    
     # First load the data 
     list_IDs = np.load(dataset, allow_pickle = True)
     
@@ -101,19 +127,33 @@ def validate_model(model, run_name, on_test_set = False):
 
     # Init evaluator object to store metrics
     save_as = run_name + '_' + data_name
+    if resize_method == tf.image.ResizeMethod.NEAREST_NEIGHBOR:
+        save_as = save_as + '_nearest'
     evaluator = Evaluator(save_after_n_samples = 1, nowcast_method = save_as)
-
+    
+    # Create directory to save predictions
+    pred_path = config.dir_pred + save_as + '/' 
+    if not os.path.exists(pred_path):
+        os.makedirs(pred_path) 
+    
+    
+    mask = get_mask_rtcor()
+    
     # zip the two generators so that the preprocessed X matches the target Y data
-    for (xs_prep, _), (_, ys) in tqdm(zip(gen, cp_gen)):
-
+    for (xs_prep, _), (_, ys), (_, ys_timestamps) in tqdm(zip(gen, cp_gen, list_IDs)):
         ys_pred = model.predict(xs_prep)        
         ys_pred = undo_prep(ys_pred, norm_method = norm_method, r_to_dbz=convert_to_dbz, 
-                            downscale256 = downscale256)
-
+                            downscale256 = downscale256, resize_method = resize_method)
         for y_pred, y_target in zip(ys_pred, ys):
             leadtimes = [30, 60, 90]
             for i, leadtime in enumerate(leadtimes):
-                R_forecast = np.array(y_pred[i])
-                R_target = np.array(y_target[i])
+                R_forecast = np.squeeze(np.array(y_pred[i])) * mask
+                R_target = np.squeeze(np.array(y_target[i]))
+                
+                # save forecast:
+                fn_forecast = ys_timestamps[i]+'_lt{}'.format(leadtime)
+                np.save(pred_path+fn_forecast, R_forecast)
+                
+                # eval
                 evaluator.verify(R_target, R_forecast, leadtime=leadtime)
 
